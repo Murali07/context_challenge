50e10a0460376e945d8bfdbe660cece80e514043
Murali07 <muraliselvaraj97@gmail.com>
2022-05-22 12:45:31 +0530

    context challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, createContext, useContext} from "react";
+import "./App.css";
 
-function App() {
+
+const themCtx = createContext()
+
+export default function App() {
+  const [mode, setMode] = useState("light");
+  const styles = {
+    background: mode === "light" ? "black" : "white"
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <themCtx.Provider value={[mode, setMode]}>    
+      <div style={styles} className="App">
+        <List setMode={setMode} mode={mode} />
+      </div>
+    </themCtx.Provider>
   );
 }
 
-export default App;
+//End Goal
+const List = () => (
+  <ul>
+    <ListItem value="light" />
+    <ListItem value="dark" />
+  </ul>
+);
+
+//End Goal
+const ListItem = ({ value}) => (
+  <li>
+    <Button value={value}  />
+  </li>
+);
+
+const Button = ({ value}) => {
+  // subscribe
+  const[mode, setMode] = useContext(themCtx)
+  
+  const styles = {
+    background: mode === "light" ? "white" : "black",
+    color: mode === "light" ? "black" : "white"
+  };
+
+  return (
+  <button 
+    style={styles}
+    onClick={() => setMode(value === "light" ? "light" : "dark")}
+  >
+    {value}
+  </button>
+  );
+};
+
+// Task (Don't delete or edit components)
+// 1. Complete with props drilling - 15mins
+// 2. Complete with useContext (avoiding props drilling)
+
+// Reference - https://codesandbox.io/s/usecontext-reference-hw839?file=/src/index.js:165-178
+// Expected output - https://csb-6qf0y.netlify.app/
+
+// 1. Creating - createContext
+// 2. Publisher - provider - context.Provider
+// 3. Subscriber - useContext - useContext(context)
